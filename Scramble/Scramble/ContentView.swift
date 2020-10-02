76dc5591909593a3c6ce0fd079870ae7f2cfb298
Abhishek Rawat <47311875+AbePlays@users.noreply.github.com>
2020-10-02 18:31:16 +0530

    Add files via upload

---
+++ Scramble/Scramble/ContentView.swift
@@ -0,0 +1,131 @@
+//
+//  ContentView.swift
+//  Scramble
+//
+//  Created by Abhishek Rawat on 02/10/20.
+//  Copyright © 2020 Abe10. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
+    @State private var score = 0
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord, onCommit: addWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                    .autocapitalization(.none)
+                
+                List(usedWords, id: \.self) {
+                    Image(systemName: "\($0.count).circle")
+                    Text($0)
+                }
+                
+                Text("Score = \(score)")
+            }
+            .navigationBarTitle(rootWord)
+            .navigationBarItems(leading: Button(action: startGame) {
+                Image(systemName: "house.fill").renderingMode(.original)
+            })
+            .onAppear(perform: startGame)
+            .alert(isPresented: $showingError) {
+                Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+            }
+        }
+    }
+    
+    func isRepeated(word : String) -> Bool {
+        if usedWords.contains(word) {
+            return true
+        }
+        
+        if word.count < 3 {
+            return true
+        }
+        
+        return false
+    }
+    
+    func isValid(word : String) -> Bool {
+        var temp = rootWord
+        
+        for letter in word {
+            if let index = word.firstIndex(of: letter) {
+                temp.remove(at: index)
+            } else {
+                return false
+            }
+        }
+        
+        return true
+    }
+    
+    func isReal(word: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        
+        return misspelledRange.location == NSNotFound
+    }
+    
+    func wordError(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+    
+    func addWord() {
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        guard answer.count > 0 else {
+            return
+        }
+        
+        guard !isRepeated(word: answer) else {
+            wordError(title: "Word used already", message: "Be more original")
+            return
+        }
+
+        guard isValid(word: answer) else {
+            wordError(title: "Word not recognized", message: "You can't just make them up, you know!")
+            return
+        }
+
+        guard isReal(word: answer) else {
+            wordError(title: "Word not possible", message: "That isn't a real word.")
+            return
+        }
+        
+        usedWords.insert(answer, at : 0)
+        score += answer.count
+        newWord = ""
+    }
+    
+    func startGame() {
+        if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
+            if let startWords = try? String(contentsOf: startWordsURL) {
+                let allWords = startWords.components(separatedBy: "\n")
+                rootWord = allWords.randomElement() ?? "silkworm"
+                score = 0
+                usedWords.removeAll()
+                return
+            }
+        }
+        fatalError("Could not load start.txt from bundle.")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
